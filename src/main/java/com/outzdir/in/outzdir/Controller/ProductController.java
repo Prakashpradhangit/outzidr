e92aa0d26491d9f513f8558b945dda22cb1800d7
prakashpradhangit <prakashpradhan5013@gmail.com>
2026-08-22 20:57:30 +0530

    pagination added to the products

--- src/main/java/com/outzdir/in/outzdir/Controller/ProductController.java
+++ src/main/java/com/outzdir/in/outzdir/Controller/ProductController.java
@@ -3,14 +3,14 @@ package com.outzdir.in.outzdir.Controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.outzdir.in.outzdir.Entity.Product;
@@ -26,6 +26,11 @@ public class ProductController {
     private final ProductRepository productRepository;
 
     @GetMapping
+    public Page<Product> getProductByPage(@RequestParam int page,@RequestParam  int size){
+        return productRepository.findAll(PageRequest.of(page, size));
+    }
+    
+    @GetMapping("/all")
     public List<Product> getAllProduct(){
         return productRepository.findAll();
     }
